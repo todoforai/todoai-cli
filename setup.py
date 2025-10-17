c285d0f0148f688c58ffa2cdb98096c6a480b52d
SixZero <havliktomi@hotmail.com>
2025-10-17 09:53:16 +0200

    Release v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="todoai-cli",
-    version="0.1.2",
+    version="0.1.3",
     author="TODOforAI",
     author_email="support@todoforai.com",
     description="Command-line interface for TODOforAI Edge",
--- todoai_cli/__init__.py
+++ todoai_cli/__init__.py
@@ -1,2 +1,2 @@
 """TODOforAI CLI Package"""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
